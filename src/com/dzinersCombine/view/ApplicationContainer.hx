7a7059d58fa78de1c415dff3af3086e0e5a721c9
saumya <2saumyaray@gmail.com>
2012-01-25 00:45:43 +0530

    wip

--- src/com/dzinersCombine/view/ApplicationContainer.hx
+++ src/com/dzinersCombine/view/ApplicationContainer.hx
@@ -1,5 +1,9 @@
 package com.dzinersCombine.view;
+
+import nme.display.Graphics;
 import nme.display.Sprite;
+
+import com.eclecticdesignstudio.motion.Actuate;
 
 /**
  * Base container of our application
@@ -9,6 +13,8 @@ import nme.display.Sprite;
 
 class ApplicationContainer extends Sprite
 {
+	private var leftBlock:Sprite ;
+	private var rightBlock:Sprite ;
 
 	public function new() 
 	{
@@ -19,6 +25,32 @@ class ApplicationContainer extends Sprite
 	private function init() 
 	{
 		trace(this);
+		this.leftBlock = new Sprite();
+		this.rightBlock = new Sprite();
+		//
+		this.addChild(this.leftBlock);
+		this.addChild(this.rightBlock);
+		//this.rightBlock.x = 400;
+		var gl:Graphics = this.leftBlock.graphics;
+		var gr:Graphics = this.rightBlock.graphics;
+		this.makeItColor(gl, 0xFF0000);
+		this.makeItColor(gr, 0xDDDDDD);
+		//
+		Actuate.tween (this.rightBlock, 1, { x:400 } );
+		//make the menu
+		var b:ButtonBase = new ButtonBase();
+		this.addChild(b);
+		b.x = 10;
+		b.y = 450;
+		b.setBg(0x00FF00);
+		b.setLabel('Home');
+	}
+	
+	private function makeItColor(g:Graphics,color:Int):Void
+	{
+		g.beginFill(color);
+		g.drawRect(0, 0, 400, 400);
+		g.endFill();
 	}
 	
 }
---
+++ src/com/dzinersCombine/view/ButtonBase.hx
@@ -0,0 +1,49 @@
+package com.dzinersCombine.view;
+import nme.display.Graphics;
+import nme.display.Sprite;
+import nme.text.TextField;
+
+/**
+ * ...
+ * @author saumya
+ */
+
+class ButtonBase extends Sprite
+{
+	private var label:TextField;
+
+	public function new() 
+	{
+		super();
+		this.init();
+	}
+	
+	private function init() 
+	{
+		var g:Graphics = this.graphics;
+		g.beginFill(0xFF0000);
+		g.drawRect(0, 0, 100, 20);
+		g.endFill();
+		//
+		this.label = new TextField();
+		this.label.width = 90;
+		this.label.height = 20;
+		this.label.text = 'Label';
+		//
+		this.addChild(this.label);
+	}
+	
+	public function setBg(color:Int):Void
+	{
+		this.graphics.clear();
+		this.graphics.beginFill(color);
+		this.graphics.drawRect(0, 0, 100, 20);
+		this.graphics.endFill();
+	}
+	
+	public function setLabel(s:String):Void
+	{
+		this.label.text = s;
+	}
+	
+}
