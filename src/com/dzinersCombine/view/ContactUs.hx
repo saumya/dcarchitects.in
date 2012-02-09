a24f38f75d76f39c9df45834a85462cad6893614
saumya <2saumyaray@gmail.com>
2012-02-09 16:49:58 +0530

    added conditional compiling options for HTML5 and Flash. This is awesome.

--- src/com/dzinersCombine/view/ContactUs.hx
+++ src/com/dzinersCombine/view/ContactUs.hx
@@ -47,44 +47,68 @@ class ContactUs extends Sprite
 			textField.embedFonts = true;
 		*/
 		//
-		/*
-		this.guestName = new TextField();
-		this.guestName.width = 150;
-		this.guestName.height = 20;
-		this.guestName.border = true;
-		this.guestName.type = TextFieldType.INPUT;
-		this.guestName.x = 100;
-		this.guestName.y = 50;
-		this.guestName.text = 'name';
-		this.addChild(this.guestName);
-		this.guestMail = new TextField();
-		this.guestMail.width = 150;
-		this.guestMail.height = 20;
-		this.guestMail.border = true;
-		this.guestMail.type = TextFieldType.INPUT;
-		this.guestMail.x = 100;
-		this.guestMail.y = 80;
-		this.guestMail.text = 'mailid@myMail.com';
-		this.addChild(this.guestMail);
-		this.guestMessage = new TextField();
-		this.guestMessage.width = 150;
-		this.guestMessage.height = 100;
-		this.guestMessage.border = true;
-		this.guestMessage.type = TextFieldType.INPUT;
-		this.guestMessage.multiline = true;
-		this.guestMessage.wordWrap = true;
-		this.guestMessage.text = 'Hi, This is my message. \n I like your work and want to reach you for a project.';
-		this.guestMessage.x = 100;
-		this.guestMessage.y = 110;
-		this.addChild(this.guestMessage);
-		this.submitBtn = new ButtonBase();
-		this.submitBtn.setLabel('Submit');
-		this.submitBtn.setBg(0xCCCCCC);
-		this.submitBtn.x = 100;
-		this.submitBtn.y = 220;
-		this.addChild(this.submitBtn);
-		*/
+		#if flash
+			this.guestName = new TextField();
+			this.guestName.width = 150;
+			this.guestName.height = 20;
+			this.guestName.border = true;
+			this.guestName.type = TextFieldType.INPUT;
+			this.guestName.x = 100;
+			this.guestName.y = 50;
+			this.guestName.text = 'name';
+			this.addChild(this.guestName);
+			this.guestMail = new TextField();
+			this.guestMail.width = 150;
+			this.guestMail.height = 20;
+			this.guestMail.border = true;
+			this.guestMail.type = TextFieldType.INPUT;
+			this.guestMail.x = 100;
+			this.guestMail.y = 80;
+			this.guestMail.text = 'mailid@myMail.com';
+			this.addChild(this.guestMail);
+			this.guestMessage = new TextField();
+			this.guestMessage.width = 150;
+			this.guestMessage.height = 100;
+			this.guestMessage.border = true;
+			this.guestMessage.type = TextFieldType.INPUT;
+			this.guestMessage.multiline = true;
+			this.guestMessage.wordWrap = true;
+			this.guestMessage.text = 'Hi, This is my message. \n I like your work and want to reach you for a project.';
+			this.guestMessage.x = 100;
+			this.guestMessage.y = 110;
+			this.addChild(this.guestMessage);
+			this.submitBtn = new ButtonBase();
+			this.submitBtn.setLabel('Submit');
+			this.submitBtn.setBg(0xCCCCCC);
+			this.submitBtn.x = 100;
+			this.submitBtn.y = 220;
+			this.addChild(this.submitBtn);
+			//
+			this.address = new TextField();
+			this.address.width = 400;
+			this.address.height = 300;
+			this.address.multiline = true;
+			this.address.wordWrap = true;
+			this.address.x = 300;
+			this.address.y = 40;
+			this.address.defaultTextFormat = this.format;
+			this.address.text = 'dziners combine architects, \n3,Samrudhi Apt., Plot no. 74, Sl no. 95, \nRight Bhusari Colony,\nOff Paud Road, Kothrud, \nPune, 411038';
+			this.addChild(this.address);
+		#elseif js
+			this.address = new TextField();
+			this.address.width = 400;
+			this.address.height = 300;
+			this.address.multiline = true;
+			this.address.wordWrap = true;
+			this.address.x = 200;
+			this.address.y = 80;
+			this.address.defaultTextFormat = this.format;
+			this.address.text = 'dziners combine architects, \n3,Samrudhi Apt., Plot no. 74, Sl no. 95, \nRight Bhusari Colony,\nOff Paud Road, Kothrud, \nPune, 411038';
+			this.addChild(this.address);
+		#else
+		#end
 		//
+		/*
 		this.address = new TextField();
 		this.address.width = 400;
 		this.address.height = 300;
@@ -97,6 +121,7 @@ class ContactUs extends Sprite
 		this.address.text = 'dziners combine architects, \n3,Samrudhi Apt., Plot no. 74, Sl no. 95, \nRight Bhusari Colony,\nOff Paud Road, Kothrud, \nPune, 411038';
 		
 		this.addChild(this.address);
+		*/
 		
 	}
 	
